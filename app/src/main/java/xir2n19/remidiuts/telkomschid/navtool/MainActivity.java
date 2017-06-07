0d47f4857902e9ee0031a6f47a9a5bd0f8e5cdfe
USER-ACER-PC <nicothecruzguardian@gmail.com>
2017-06-07 23:49:26 +0700

    Final Task

---
+++ app/src/main/java/xir2n19/remidiuts/telkomschid/navtool/MainActivity.java
@@ -0,0 +1,41 @@
+package xir2n19.remidiuts.telkomschid.navtool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //nambah iki
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, tab.class));
+            }
+        });
+        // end
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, swing.class));
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, spin.class));
+            }
+        });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, drawer.class));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/xir2n19/remidiuts/telkomschid/navtool/secFragment.java
@@ -0,0 +1,26 @@
+package xir2n19.remidiuts.telkomschid.navtool;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class secFragment extends Fragment {
+
+
+    public secFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sec, container, false);
+    }
+
+}
